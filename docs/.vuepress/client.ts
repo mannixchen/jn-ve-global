bafdd050d35da5d4fa1c926697c650b47aaa37a0
Zyunchao <18651805393@163.com>
2023-05-04 15:07:24 +0800

    fix: docs 打包时, window ssr 问题;

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -2,17 +2,22 @@ import { defineClientConfig } from '@vuepress/client'
 import ElementPlus from 'element-plus'
 import zhCn from './zh-cn'
 import 'element-plus/dist/index.css'
-import GlobalComponent from 'jn-ve-global/packages/register'
-import { getGlobal } from '@jsjn/utils'
+// import GlobalComponent from 'jn-ve-global/packages/register'
+// import GlobalComponent from 'jn-ve-global'
+// import 'jn-ve-global/style.css'
 
 export default defineClientConfig({
-    enhance({ app, router, siteData }) {
-        app.directive('auth', {})
-        app.use(ElementPlus, { locale: zhCn })
-        app.use(GlobalComponent)
+    async enhance({ app, router, siteData }) {
+        // app.directive('auth', {})
+        // app.use(ElementPlus, { locale: zhCn })
+        // app.use(GlobalComponent)
 
         if (!__VUEPRESS_SSR__) {
-            getGlobal()
+            const GlobalComponent = await import('jn-ve-global/packages/register')
+            app.directive('auth', {})
+            app.use(ElementPlus, { locale: zhCn })
+            app.use(GlobalComponent.default)
+            // app.use(GlobalComponent)
         }
     },
     setup() {},
