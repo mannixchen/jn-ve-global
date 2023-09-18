12d0019b9f3a4309e441ec2504f206f922619600
Zyunchao <18651805393@163.com>
2023-09-18 09:50:35 +0800

    refactor(luckysheet): luckysheet 资源懒加载方式优化;

--- sheet/src/components/loadSource.ts
+++ sheet/src/components/loadSource.ts
@@ -1,45 +1,23 @@
 import { getGlobal, global } from '@jsjn/utils'
+import { loadCss, loadScript } from '@jsjn/utils'
 
-export function loadSource() {
-    if (window.luckysheet) return
-    const rootLocation = global.location
-    const prefix: string = `${rootLocation.origin}${rootLocation.pathname}lib/luckysheet`
+const luckyDir = '/luckysheet'
+const cssList = [
+    '/plugins/css/pluginsCss.css',
+    '/plugins/plugins.css',
+    '/css/luckysheet.css',
+    '/assets/iconfont/iconfont.css'
+]
 
-    const cssList = [
-        '/plugins/css/pluginsCss.css',
-        '/plugins/plugins.css',
-        '/css/luckysheet.css',
-        '/assets/iconfont/iconfont.css'
-    ]
+export async function loadSource() {
+    const sheetGlobalName = 'luckysheet'
+    if (window[sheetGlobalName]) return window[sheetGlobalName]
 
-    return new Promise((rootResolve) => {
-        setTimeout(() => {
-            cssList.forEach((file) => {
-                const linkTag = window.document.createElement('link')
-                linkTag.rel = 'stylesheet'
-                linkTag.href = `${prefix}${file}`
-                window.document.head.appendChild(linkTag)
-            })
-
-            new Promise((resolve, reject) => {
-                const scriptTag = window.document.createElement('script')
-                scriptTag.type = 'text/javascript'
-                // scriptTag.async = false
-                scriptTag.src = `${prefix}/plugins/js/plugin.js`
-                window.document.head.appendChild(scriptTag)
-                scriptTag.onload = (e) => {
-                    resolve(e)
-                }
-            }).then(() => {
-                const scriptTag = window.document.createElement('script')
-                scriptTag.type = 'text/javascript'
-                // scriptTag.async = false
-                scriptTag.src = `${prefix}/luckysheet.umd.js`
-                window.document.head.appendChild(scriptTag)
-                scriptTag.onload = (e) => {
-                    rootResolve(e)
-                }
-            })
-        }, 10)
+    cssList.forEach((file) => {
+        loadCss(`${luckyDir}/${file}`)
     })
+
+    await loadScript(`${luckyDir}/plugins/js/plugin.js`, null)
+    const luckysheet = await loadScript(`${luckyDir}/luckysheet.umd.js`, sheetGlobalName)
+    return luckysheet
 }
