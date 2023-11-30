ec9f6c8cead6cc45f41af550a989adf4c0ad46dd
Zyunchao <18651805393@163.com>
2023-11-30 17:16:30 +0800

    feat: 增加全局组件类型映射文件脚本;

--- jn-ve-global/scripts/build/index.ts
+++ jn-ve-global/scripts/build/index.ts
@@ -2,11 +2,15 @@
  * @Author: Zyunchao 18651805393@163.com
  * @Date: 2023-11-06 15:08:48
  * @LastEditors: Zyunchao 18651805393@163.com
- * @LastEditTime: 2023-11-30 14:43:03
+ * @LastEditTime: 2023-11-30 17:04:37
  * @FilePath: /@jsjn-librar-monorepo/jn-ve-global/scripts/build/index.ts
  * @Description: gulp 启动任务流
  */
 import { parallel, series } from 'gulp'
-import { buildModules, clean, generateTypes, buildFull } from './task'
+import { buildModules, clean, generateTypes, buildFull, generateGlobalTypes } from './task'
 
-export default series(clean, parallel(buildModules, buildFull), generateTypes)
+export default series(
+    clean,
+    parallel(buildModules, buildFull),
+    parallel(generateTypes, generateGlobalTypes)
+)
