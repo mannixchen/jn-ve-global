80168ad6b39fed5ec54a8fc5421b137e7c906a83
Zyunchao <18651805393@163.com>
2024-01-24 11:38:14 +0800

    test: 增加 pdf.js 预览功能资源

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -89,7 +89,7 @@ export default defineConfig({
         host: '0.0.0.0',
         proxy: {
             '/proxy': {
-                target: 'http://172.31.33.84',
+                target: 'http://172.31.33.70',
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/proxy/, '')
             }
