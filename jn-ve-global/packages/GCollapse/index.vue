7f2c2f0cd829800dde3c832c021f462547606be5
Zyunchao <18651805393@163.com>
2024-03-26 17:51:02 +0800

    style(components): cpllapse-nav-bars style update

--- jn-ve-global/packages/GCollapse/index.vue
+++ jn-ve-global/packages/GCollapse/index.vue
@@ -12,7 +12,7 @@
 
     <ul
         v-if="collapseItemModels?.length"
-        :class="['cpllapse-nav-bars__wrapper', { 'is-packup': isPackup }]"
+        :class="['cpllapse-nav-bars__wrapper', { 'is-packup': isPackup }, collapsStyleMode]"
     >
         <ElScrollbar ref="anchorWrapperRef" :max-height="anchorWrapperMaxH">
             <template v-for="item in collapseItemModels" :key="item.top">
@@ -23,6 +23,7 @@
         </ElScrollbar>
 
         <li class="back-top" @click="backTop">
+            <LGIcon v-if="collapsStyleMode === 'syb'" icon="el-Download" class="back-top-icon" />
             <span>返回顶部</span>
         </li>
 
@@ -47,6 +48,7 @@ import modeKey from './constant/modeKey'
 import { ElCollapse, ElScrollbar } from 'element-plus'
 import useAnchor from './hooks/useAnchor'
 import { GIcon as LGIcon } from '../GIcon'
+import { getCollapseMode } from '../_globalConstant/CollapseMode'
 
 const props = withDefaults(
     defineProps<{
@@ -77,6 +79,9 @@ const props = withDefaults(
         parent: null
     }
 )
+
+// 样式模式
+const collapsStyleMode = getCollapseMode()
 
 const emits = defineEmits(['update:modelValue'])
 
@@ -120,6 +125,8 @@ const {
     --bars-w: 160px;
     --bar-radius: 6px;
     --packup-w: 40px;
+    --trigger-size: 25px;
+    --trigger-color: #4e5966;
 
     position: fixed;
     top: 55%;
@@ -183,20 +190,118 @@ const {
             }
 
             span {
-                width: 25px;
-                height: 25px;
-                line-height: 25px;
+                width: var(--trigger-size);
+                height: var(--trigger-size);
+                line-height: var(--trigger-size);
                 text-align: center;
-                border: 2px solid #4e5966;
+                border: 2px solid var(--trigger-color);
                 border-radius: 50%;
-                color: #4e5966;
+                color: var(--trigger-color);
                 transition: all 0.2s;
             }
 
             &:hover {
                 span {
-                    color: #558dfd;
-                    border-color: #558dfd;
+                    --trigger-color: #558dfd;
+                }
+            }
+        }
+    }
+
+    &.syb {
+        --bars-w: 150px;
+
+        background: #ffffff;
+        box-shadow: 0px 0px 10px 0px rgba(201, 201, 201, 0.5);
+        border-radius: 6px 0px 0px 6px;
+
+        &.is-packup {
+            box-sizing: border-box;
+            transform: translate(var(--bars-w), -50%);
+            padding-left: 0;
+        }
+
+        li {
+            font-size: 14px;
+            text-align: left;
+            padding-left: 26px;
+            font-family: PingFangSC-Regular;
+            font-weight: 400;
+            position: relative;
+
+            &:hover,
+            &.is-active {
+                background-color: transparent;
+                color: #0091ff;
+            }
+
+            &.is-active {
+                font-weight: 600;
+
+                &::before {
+                    content: '';
+                    width: 10px;
+                    height: 10px;
+                    border-radius: 50%;
+                    background: transparent;
+                    border: 2px solid rgba(0, 145, 255, 1);
+                    display: block;
+                    position: absolute;
+                    top: 50%;
+                    transform: translateY(-50%);
+                    left: 10px;
+                    box-sizing: border-box;
+                }
+            }
+        }
+
+        .back-top {
+            border-top: 1px solid #f7f7f7;
+            color: #0091ff;
+            letter-spacing: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border-radius: 0;
+            padding: 0;
+
+            span {
+                margin-left: 4px;
+            }
+
+            .back-top-icon {
+                font-size: 18px;
+                transform: rotate(180deg);
+            }
+        }
+
+        .packup-trigger {
+            --trigger-w: 26px;
+
+            padding: 0 !important;
+            align-items: center;
+            justify-content: center;
+            position: absolute;
+            top: 50%;
+            transform: translateY(-50%);
+            right: 0;
+            left: calc(-1 * var(--trigger-w));
+            width: var(--trigger-w);
+            height: 60px;
+            box-shadow: var(--el-box-shadow-dark);
+            background-image: linear-gradient(180deg, #3aaaff 0%, #0493ff 99%);
+            border-radius: 6px 0 0 6px;
+            z-index: -1;
+
+            span {
+                --trigger-color: #fff;
+                --trigger-size: 18px;
+                font-size: 12px !important;
+            }
+
+            &:hover {
+                span {
+                    --trigger-color: #fff;
                 }
             }
         }
