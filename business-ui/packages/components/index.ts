a4599e3b7d57307fb74e9e88f56d0f15fd3efb7f
“zhujin” <zhujin@jsjngf.com>
2024-05-30 15:42:13 +0800

    fix: businessUI 组件注册修改

--- business-ui/packages/components/index.ts
+++ business-ui/packages/components/index.ts
@@ -2,7 +2,7 @@
  * @Author: “zhujin” zhujin@jsjngf.com
  * @Date: 2024-03-12 13:54:53
  * @LastEditors: “zhujin” zhujin@jsjngf.com
- * @LastEditTime: 2024-05-22 14:19:56
+ * @LastEditTime: 2024-05-30 11:25:12
  * @FilePath: \@jsjn-librar-monorepo\business-ui\packages\components\index.ts
  * @Description: 
  * 
@@ -21,13 +21,34 @@ import { BiSelectDepartment } from './select-department'
 import { BiDetail } from './detail'
 import { BiForm } from './form'
 
-
+import { BiFiAdvanceInput } from './input'
+import { BiFiInputNumber } from './input-number'
+import { BiFiInputNumberRange } from './input-number-range'
+import { BiFiInputYearRange } from './input-year-range'
 import { BiFiInputCard } from './input-card'
+import { BiFiInputSocialCode } from './input-social-code'
+import { BiFiInputPhone } from './input-phone'
 import { BiFiInputEmail } from './input-email'
+import { BiFiCheckbox } from './checkbox'
+import { BiFiRadio } from './radio'
 import { BiFiDetail } from './detail'
+import { BiFiColorPicker } from './color-picker'
+import { BiFiRate } from './rate'
+import { BiFiSlider } from './slider'
+import { BiFiSwitch } from './switch'
+import { BiFiDatePicker } from './date-picker'
+import { BiFiTimePicker } from './time-picker'
+import { BiFiTimeSelect } from './time-select'
+import { BiFiSelect } from './select'
+import { BiFiTransfer } from './transfer'
+import { BiFiCascader } from './cascader'
+import { BiFiTreeSelect } from './tree-select'
+import { BiFiAddress } from './address'
+import { BiFiUpload } from './upload'
 
 
 
+export * from './input'
 export * from './input-number'
 export * from './input-number-range'
 export * from './input-year-range'
@@ -40,6 +61,19 @@ export * from './radio'
 export * from './select-department'
 export * from './detail'
 export * from './form'
+export * from './color-picker'
+export * from './rate'
+export * from './slider'
+export * from './switch'
+export * from './date-picker'
+export * from './time-picker'
+export * from './time-select'
+export * from './select'
+export * from './transfer'
+export * from './cascader'
+export * from './tree-select'
+export * from './address'
+export * from './upload'
 
 
 export const components = [
@@ -56,8 +90,28 @@ export const components = [
     BiDetail,
     BiForm,
 
-
+    BiFiAdvanceInput,
+    BiFiInputNumber,
+    BiFiInputNumberRange,
+    BiFiInputYearRange,
     BiFiInputCard,
+    BiFiInputSocialCode,
+    BiFiInputPhone,
     BiFiInputEmail,
-    BiFiDetail
+    BiFiCheckbox,
+    BiFiRadio,
+    BiFiDetail,
+    BiFiColorPicker,
+    BiFiRate,
+    BiFiSlider,
+    BiFiSwitch,
+    BiFiDatePicker,
+    BiFiTimePicker,
+    BiFiTimeSelect,
+    BiFiSelect,
+    BiFiTransfer,
+    BiFiCascader,
+    BiFiTreeSelect,
+    BiFiAddress,
+    BiFiUpload
 ]
